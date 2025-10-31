6dac94bc862570913cfd983e788999a2b225df70
atbalaji <balaji2765@gmail.com>
2025-10-31 03:13:36 +0000

    Implemented Axios interceptors to set http headers and to transform response

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import initializeAxios from "apis/axios";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -7,6 +8,7 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+initializeAxios();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
